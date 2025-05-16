2cd747fbfbf2e0966523b2c43b4a55d133d651b7
墨渊 <3311909693@qq.com>
2025-05-17 00:59:28 +0800

    修改技能变身bug

--- saveSkinParams.js
+++ saveSkinParams.js
@@ -5,13 +5,93 @@ window.saveFunc = function(lib, game, ui, get, ai, _status){window.skinSwitch.sa
             "qianjing": {
                 "x": [
                     0,
-                    0.6
+                    0.61
                 ],
                 "y": [
                     0,
-                    0.36
+                    0.28
                 ],
                 "scale": 0.21,
+                "angle": 0
+            }
+        }
+    },
+    "re_zhonghui": {
+        "潜蛟觊天": {
+            "gongji": {
+                "x": [
+                    0,
+                    0.48
+                ],
+                "y": [
+                    0,
+                    0.56
+                ],
+                "scale": 0.32,
+                "angle": 0
+            }
+        }
+    },
+    "re_fengfangnv": {
+        "窗影镜花": {
+            "gongji": {
+                "x": [
+                    0,
+                    0.82
+                ],
+                "y": [
+                    0,
+                    0.11
+                ],
+                "scale": 0.73,
+                "angle": 0
+            }
+        }
+    },
+    "zhangjinyun": {
+        "金蛟巧刻": {
+            "gongji": {
+                "x": [
+                    0,
+                    0.76
+                ],
+                "y": [
+                    0,
+                    0.26
+                ],
+                "scale": 0.57,
+                "angle": 0
+            }
+        }
+    },
+    "sb_xiaoqiao": {
+        "丽质天成": {
+            "gongji": {
+                "x": [
+                    0,
+                    0.52
+                ],
+                "y": [
+                    0,
+                    0.52
+                ],
+                "scale": 0.61,
+                "angle": 0
+            }
+        }
+    },
+    "shen_zhaoyun": {
+        "鳞振九霄": {
+            "gongji": {
+                "x": [
+                    0,
+                    0.5
+                ],
+                "y": [
+                    0,
+                    0.5
+                ],
+                "scale": 0.57,
                 "angle": 0
             }
         }
